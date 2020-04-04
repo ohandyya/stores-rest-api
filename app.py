507c7adec12075ec56c394aa0dd4142f74b4bb68
atseng <atseng@pindropsecurity.com>
2020-04-04 11:26:42 -0400

    Fixed not importing db

--- app.py
+++ app.py
@@ -15,11 +15,6 @@ app.secret_key = "andy_secret"
 api = Api(app)
 
 
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-
 jwt = JWT(app, authenticate, identity)
 
 api.add_resource(Item, "/item/<string:name>")
